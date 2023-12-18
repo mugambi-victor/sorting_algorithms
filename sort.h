6e5b8db6348bde02c334d773563ca764d984d2d9
mugambi-victor <victormmugambi@gmail.com>
2023-12-19 01:26:07 +0300

    task 4

--- sort.h
+++ sort.h
@@ -22,5 +22,8 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quicksort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 
 #endif
